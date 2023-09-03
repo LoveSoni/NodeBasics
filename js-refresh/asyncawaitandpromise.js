1e8265354abc59661ab27b54877bc8d5c86cfb56
Love <lovesoni607@gmail.com>
2023-09-03 16:24:55 +0530

    async await

---
+++ js-refresh/asyncawaitandpromise.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+const myPromise = new Promise((resolve, reject) => {
+    let exception = false;
+    if (exception != true) {
+        setTimeout(() => {
+            console.log('hello');
+        }, 2000)
+        resolve('executed');
+    }
+    else {
+        reject('failure');
+    }
+});
+
+myPromise.then((msg) => { console.log(msg) }).catch(function (msg) { console.log(msg) });
