6c1047bc27cdf6e79a1477a7f19ea4b0b53f53be
Love <lovesoni607@gmail.com>
2023-09-03 13:13:46 +0530

    variables and functions

---
+++ js-refresh/var.js
@@ -0,0 +1,12 @@
+var name = 'love';
+var age = 26;
+var isLoggedIn = true;
+console.log(name);
+console.log(age);
+console.log(isLoggedIn);
+
+function user(name, email) {
+    return `username is ${name} and email is ${email}`;
+}
+
+console.log(user('love', 'love.love@gmail.com'));
