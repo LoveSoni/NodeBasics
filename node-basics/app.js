b761aee203454c0f34a8fa6bda7d69e619037823
Love <lovesoni607@gmail.com>
2023-09-03 19:18:18 +0530

    app.js

--- node-basics/app.js
+++ node-basics/app.js
@@ -1,13 +1,16 @@
 const http = require('http');
 
-function reqListener(request, response) {
+// function reqListener(request, response) {
 
-}
+// }
 
 // http.createServer(reqListener)
 // or define reqListner callback function in createserver only
 const server = http.createServer((request, response) => {
-    console.log(request);
+    console.log('request is :', request);
+    console.log('response is :', response);
 });
+
 // console.log(server.address().port);
 server.listen(3000);
+process.exit();
