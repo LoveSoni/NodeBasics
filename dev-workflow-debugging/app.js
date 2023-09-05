001676b86bf30132f06599b6afd17d4736dfc2db
Love <lovesoni607@gmail.com>
2023-09-05 23:44:35 +0530

    added nodemon dependency

--- dev-workflow-debugging/app.js
+++ dev-workflow-debugging/app.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.statusCode = 200;
-    response.end('hey');
+    response.end('heysdf upated');
 })
 
 server.listen(3000);
