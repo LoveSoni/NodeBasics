fe3a73055a712114912b7ce6de72b4d32013abe7
Love <lovesoni607@gmail.com>
2023-09-05 21:53:25 +0530

    route rquest in different file

--- node-basics/routingrequest.js
+++ node-basics/routingrequest.js
@@ -1,44 +1,6 @@
 'use strict'
 const http = require('http');
-const fs = require('fs');
+const route = require('./route.js')
 
-const server = http.createServer((request, response) => {
-    const url = request.url;
-    console.log('url is ', request.url);
-    console.log('method is', request.method);
-    const method = request.method;
-    if (url === '/') {
-        response.write('<html>')
-        response.write('<head>')
-        response.write('</head>')
-        response.write('<body>')
-        response.write("<form action='/message' method='post'><input type='text' name='capturedmessage'></input><button id='send'>Send</button></form>")
-        response.write('</body>');
-        response.write('</html>');
-        response.end();
-    }
-
-    else if (url === '/message' && method === 'POST') {
-        console.log('inside method url');
-        let requestBody = [];
-        request.on('data', function (datachunk) {
-            console.log('chunk ', datachunk);
-            requestBody.push(datachunk);
-        })
-        console.log('data chunk is : ', requestBody)
-        request.on('end', () => {
-            const parsedBody = Buffer.concat(requestBody).toString();
-            console.log('parsed body is ', parsedBody);
-            fs.writeFileSync('./capturedinputtexst.txt', parsedBody, (error) => {
-                response.statusCode = 302;
-                // redirecting url
-                response.setHeader('Location', '/');
-                response.end();
-            });
-
-        })
-
-
-    }
-})
+const server = http.createServer(route);
 server.listen(3000);
