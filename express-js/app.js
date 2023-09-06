9357d6d8a158c1e11730c4c1f88de13ed97191cf
Love <lovesoni607@gmail.com>
2023-09-06 23:53:22 +0530

    using request middleware using express

--- express-js/app.js
+++ express-js/app.js
@@ -5,3 +5,20 @@ const expressimport = require('express');
 //our express library exports a function, so we have added () to invoke that
 
 const app = expressimport();
+// here app is the valid request/response handler so we can directly pass it to createServer function
+
+app.use((request, response, next) => {
+    console.log('in my middleware')
+    response.statusCode = 200;
+    // response.end();
+    next(); // this allows request to contine to the next middleware in line
+})
+
+// we can have multiple app request handler e.g. 
+// but this won't be called until we have next() call in the first request handler
+app.use((request, response, next) => {
+    console.log('in another middleware')
+})
+
+const server = http.createServer(app);
+server.listen(3000);
