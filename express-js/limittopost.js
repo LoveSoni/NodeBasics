5f53fe75750ad54f802298ca64895345b8b5394f
Love <lovesoni607@gmail.com>
2023-09-07 23:35:07 +0530

    limit to http request in express

---
+++ express-js/limittopost.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));// need to add to parse request body
+
+app.use("/addProduct", (request, response, next) => {
+    response.send("<form action='/product' method='POST'><input type='text' name='title'></input><button>Add Product</button></form>")
+})
+
+app.post("/product", (req, response, next) => {
+    // console.log('request body is : ', request.body);  will have to parse the body otherwise it won't work
+    // and we need to add a plugin i.e. npm install --save body-parser
+    console.log(req.body['title']);
+    response.redirect('/');
+})
+
+app.use("/", (request, response, next) => {
+    response.send('handling all ')
+})
+
+
+app.listen(3000);
