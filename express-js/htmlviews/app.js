0f3925a6110c950dc5116223e21b664b7aee2349
Love <lovesoni607@gmail.com>
2023-09-09 00:10:28 +0530

    refactored html views with router

--- express-js/htmlviews/app.js
+++ express-js/htmlviews/app.js
@@ -1,23 +1,21 @@
 'use strict'
 const express = require('express');
 const bodyParser = require('body-parser');
+const addproductroute = require('./addproductroute');
+const shoproute = require('./shoproute');
 const path = require('path');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/add-product', (request, response, next) => {
-    response.sendFile(path.join(__dirname, 'addproduct.html'))
-})
 
-app.get('/', (request, response, next) => {
-    console.log(path.join(__dirname));
-    response.sendFile(path.join(__dirname, 'shop.html'));
-})
+app.use(addproductroute);
+app.use(shoproute);
 
 app.use((request, response, next) => {
-    response.status(404).sendFile(path.join(__dirname, '404notfound.html'))
+    response.sendFile(path.join(__dirname, '404notfound.html'));
 })
+
 
 app.listen(3000);
