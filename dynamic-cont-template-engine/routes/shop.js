1f06a582ffe3d18536224542e6230b50c88231eb
Love <lovesoni607@gmail.com>
2023-09-10 14:39:17 +0530

    handlebars integrated

--- dynamic-cont-template-engine/routes/shop.js
+++ dynamic-cont-template-engine/routes/shop.js
@@ -9,7 +9,7 @@ router.get('/', (request, response, next) => {
     console.log('Addded products are : ', adminData.productsList);
     // response.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
     // to use pug file we have to use the render method
-    response.render('shop', { prods: adminData.productsList });// we don't to specify the full path(including folder) because we have set it already in our app.js file under views key (app.set('views', 'views');)
+    response.render('shop', { prods: adminData.productsList, hasProds: adminData.productsList.length > 0 });// we don't to specify the full path(including folder) because we have set it already in our app.js file under views key (app.set('views', 'views');)
 });
 
 module.exports = router; 
