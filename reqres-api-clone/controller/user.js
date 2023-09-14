05fcd2085866b1dc4788181580de8ae8e7b268e2
Love <lovesoni607@gmail.com>
2023-09-14 19:46:31 +0530

    get specific user api

--- reqres-api-clone/controller/user.js
+++ reqres-api-clone/controller/user.js
@@ -11,4 +11,15 @@ exports.getUerList = (request, response, next) => {
 exports.getUserDetails = (request, response, next) => {
     const userId = request.params.userId;
     console.log('user id ', userId);
+    fs.readFile(usersListJsonFile, (error, fileContent) => {
+        const userData = JSON.parse(fileContent).data;
+        const userIdData = (userData.filter((usrObject) => { return (usrObject.id == userId) })).map((data) => JSON.
+            stringify(data));
+        console.log('useriddata', userIdData != null);
+        if (userIdData.length) {
+            const userIdDataObject = JSON.parse(userIdData); response.send(userIdDataObject);
+        } else {
+            response.header('Content-Type', 'application/json').send('{}')
+        }
+    });
 }
