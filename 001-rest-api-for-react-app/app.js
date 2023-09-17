b12d07f66d0af415b3dc8cac06574e82f2787b7f
Love <lovesoni607@gmail.com>
2023-09-17 19:04:59 +0530

    create post and get post

--- 001-rest-api-for-react-app/app.js
+++ 001-rest-api-for-react-app/app.js
@@ -4,6 +4,12 @@ const feedRoute = require('./routes/feed');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use('/feed', feedRoute.feedRouter);
 
 
--- 001-rest-api-for-react-app/controller/feed.js
+++ 001-rest-api-for-react-app/controller/feed.js
@@ -1,8 +1,27 @@
 exports.getPosts = (request, response, next) => {
-
+    response.status(200).json({
+        posts: [{
+            _id: '1',
+            title: 'first post',
+            message: 'message from first post',
+            imageUrl: '../data/duck.png',
+            creator: {
+                name: 'love'
+            },
+            createdAt: new Date().toISOString()
+        }
+        ]
+    })
 };
 
 
 exports.createPost = (request, response, next) => {
+    const title = request.body.title;
+    const content = request.body.content;
+
+    response.status(200).json({
+        message: 'Post Created Successfully!',
+        post: { _id: new Date().toISOString, title: title, message: content, creator: { name: 'love' }, createdAt: new Date().toISOString() }
+    })
 
 }
