10493a8793175f446fd32b2e432710344cca3000
Love <lovesoni607@gmail.com>
2023-09-17 19:44:39 +0530

    server side validation

--- 001-rest-api-for-react-app/controller/feed.js
+++ 001-rest-api-for-react-app/controller/feed.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator')
 exports.getPosts = (request, response, next) => {
     response.status(200).json({
         posts: [{
@@ -18,7 +19,14 @@ exports.getPosts = (request, response, next) => {
 exports.createPost = (request, response, next) => {
     const title = request.body.title;
     const content = request.body.content;
-
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+        return response.status(422).json({
+            message: 'validation failed! Entered data is incorrred',
+            errors: errors.array()
+        })
+    }
+    console.log('ERRORS ARE ', errors);
     response.status(200).json({
         message: 'Post Created Successfully!',
         post: { _id: new Date().toISOString, title: title, message: content, creator: { name: 'love' }, createdAt: new Date().toISOString() }
--- 001-rest-api-for-react-app/routes/feed.js
+++ 001-rest-api-for-react-app/routes/feed.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const feedController = require('../controller/feed');
+const { body } = require('express-validator');
 
 const router = express.Router();
 
 router.get('/posts', feedController.getPosts);
 
-router.post('/post', feedController.createPost);
-
+router.post('/post', [
+    body('title')
+        .trim()
+        .isLength({ min: 5 }),
+    body('content')
+        .trim()
+        .isLength({ min: 5 })
+], feedController.createPost);
 
 exports.feedRouter = router;
